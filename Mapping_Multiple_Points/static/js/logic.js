a567842d588a83364d1a57d434df050934e0a955
Jason Hilberdink <jhilberdink@gmail.com>
2021-05-17 19:15:31 -0400

    created map with multiple points

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -8,15 +8,23 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/dark-v10',
+    id: 'mapbox/streets-v11',
     accessToken: API_KEY
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-// Add a marker for Los Angeles, California
-L.circleMarker([34.0522, -118.2437], { 
-    color: "black",
-    fillColor: '#ffffa1',
-    radius: 300
-}).addTo(map);
+// Get data from the cities.js file
+let cityData = cities;
+
+
+// Loop through the array and add a marker for each city to the map
+cityData.forEach(function(city) {
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+    .addTo(map);
+    console.log(city)
+});
+
