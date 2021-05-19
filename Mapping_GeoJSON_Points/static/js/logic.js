61d50e5095ec61494db79a1c4102016edd183e54
Jason Hilberdink <jhilberdink@gmail.com>
2021-05-19 15:44:16 -0400

    mapped GeoJSON point type

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -2,13 +2,13 @@
 console.log("working");
 
 // Create a map object with a center and a zoom level
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/light-v10',
+    id: 'mapbox/dark-v10',
     accessToken: API_KEY
 });
 // Then we add our 'graymap' tile layer to the map.
@@ -28,16 +28,45 @@ streets.addTo(map);
 //     console.log(city)
 // });
 
-// coordinates for each plot to be used in the line
-let line = [
-    [33.9416, -118.4085],
-    [30.1975, -97.6664],
-    [43.6777, -79.624],
-    [39.8729, -75.2437]
-];
+// // coordinates for each plot to be used in the line
+// let line = [
+//     [33.9416, -118.4085],
+//     [30.1975, -97.6664],
+//     [43.6777, -79.624],
+//     [39.8729, -75.2437]
+// ];
 
-// create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-    color: "lightblue",
-    dashArray: 4
+// // create a polyline using the line coordinates and make the line red.
+// L.polyline(line, {
+//     color: "lightblue",
+//     dashArray: 4
+// }).addTo(map);
+
+// Add GeoJSON data
+let sanFranAirport = 
+{"type": "FeatureCollection", "features":[{
+    "type": "Feature",
+    "properties": {
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+    
+]}
+
+// Grabbing our GeoJSON data
+L.geoJSON(sanFranAirport, {
+    onEachFeature: function(feature, layer) {
+        console.log(layer);
+        layer.bindPopup("<h2>" + feature.properties.faa + "</h2>");
+        }
 }).addTo(map);
